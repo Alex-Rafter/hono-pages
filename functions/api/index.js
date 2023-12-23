17783212a20b38c2e798993176d66c5d94d91c87
Alex Rafter <alexrafteremail@gmail.com>
2023-12-23 09:26:08 +0000

    bookstore binding

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,10 +1,6 @@
 export async function onRequest(context) {
-    // const cssFile = await context.env.BSCSS.get("bs-min")
-    // const newResponse = new Response(cssFile);
-    // const test = await Object.keys(context.env.BOOKSTORE)
-    // const newResponse = new Response(`${test}`);
-    // return newResponse;
-    const ps = context.env.BOOKSTORE.prepare('SELECT * FROM books')
+    // bookstore
+    const ps = context.env.BOOKSTORE_DB.prepare('SELECT * FROM books')
     const data = await ps.first();
     return new Response.json(data);
 }
