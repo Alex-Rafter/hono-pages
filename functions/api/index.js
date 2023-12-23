3aab35bfd00208a92cc650c81813f10665ac5c7d
Alex Rafter <alexrafteremail@gmail.com>
2023-12-23 08:51:12 +0000

    test with kv store

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,5 +1,8 @@
 export async function onRequest(context) {
-    const ps = context.env.BOOKSTORE.prepare('SELECT * FROM tablename;');
-    const data = await ps.first();
-    return new Response.json(data);
+    const cssFile = await env.BSCSS.get("bs-min")
+    const newResponse = new Response(cssFile);
+    return newResponse;
+    // const ps = context.env.BOOKSTORE.prepare('SELECT * FROM tablename;');
+    // const data = await ps.first();
+    // return new Response.json(data);
 }
